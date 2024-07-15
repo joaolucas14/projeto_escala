4b662065b48fa1a1280aca067496a10b4dc7c538
joaolucas14 <jlparceiro10@gmail.com>
2024-07-15 13:52:53 -0300

    criando a model ministro e a página de login

--- escala/models.py
+++ escala/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Ministro(models.Model):
+    nome = models.CharField(max_length=25, null=False, blank=False)
+    sobrenome = models.CharField(max_length=25, null=False, blank=False)
+    telefone = models.CharField(max_length=20, null=False, blank=False)
+    email = models.EmailField(null=True)
--- escala/views.py
+++ escala/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView 
+from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView
+from .models import Ministro
 # Create your views here.
 
 
 class Home(TemplateView):
     template_name = "escala/home.html"
+
+
+class Login(LoginView):
+    model = Ministro
+    fields = [
+        "nome",
+        "sobrenome",
+        "telefone",
+        "email",
+    ]
