9d4b8309057b7276a0928789523590da50d446dc
joaolucas14 <jlparceiro10@gmail.com>
2024-07-17 14:17:18 -0300

    alterando o label do formulário de login

--- escala/forms.py
+++ escala/forms.py
@@ -3,5 +3,5 @@ from django.contrib.auth.forms import AuthenticationForm
 
 
 class CustomLoginForm(AuthenticationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Usuário'}), label="Usuário")
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control' , 'placeholder': 'Senha'}), label="Senha")
