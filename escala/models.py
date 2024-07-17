58716f908b3c20e3032d783c1e952b506092dacb
joaolucas14 <jlparceiro10@gmail.com>
2024-07-17 14:09:21 -0300

    criando a página de login

--- escala/models.py
+++ escala/models.py
@@ -1,8 +1,1 @@
 from django.db import models
-
-
-class Ministro(models.Model):
-    nome = models.CharField(max_length=25, null=False, blank=False)
-    sobrenome = models.CharField(max_length=25, null=False, blank=False)
-    telefone = models.CharField(max_length=20, null=False, blank=False)
-    email = models.EmailField(null=True)
--- escala/views.py
+++ escala/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from django.contrib.auth.views import LoginView
-from .models import Ministro
-# Create your views here.
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.views.generic import FormView
+from .forms import CustomLoginForm
 
 
 class Home(TemplateView):
     template_name = "escala/home.html"
 
 
-class Login(LoginView):
-    model = Ministro
-    fields = [
-        "nome",
-        "sobrenome",
-        "telefone",
-        "email",
-    ]
+class CustomLoginView(FormView):
+    template_name = 'registration/login.html'
+    form_class = CustomLoginForm
+    success_url = reverse_lazy('home')  
+
+    def form_valid(self, form):
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(self.request, username=username, password=password)
+        if user is not None:
+            login(self.request, user)
+            return HttpResponseRedirect(self.get_success_url())
+        return super().form_invalid(form)
